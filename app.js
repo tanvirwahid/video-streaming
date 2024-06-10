8b8d47124ab30ae44b4e98b38edf3246dd579c6f
Tanvir <nevillongbottom33@gmail.com>
2024-06-10 12:28:07 +0600

    initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const bodyParser = require('body-parser');
+const routes = require('./routes/index');
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+app.use('/api', routes);
+
+app.listen(port, () => {
+    console.log(`Listening to port :${port}`);
+});
